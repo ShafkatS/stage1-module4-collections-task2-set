dc9792be97368d16f4f04760fab66883aac3419c
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-19 16:54:34 +0500

    Update HashSetCreator.java

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,23 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet set = new HashSet();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            set.add(sourceList.get(i));
+            int temp = 0;
+            if (sourceList.get(i) % 2 == 0) {
+                temp = sourceList.get(i);
+                while (temp % 2 == 0) {
+
+                    temp = temp / 2;
+                    set.add(temp);
+                }
+            } else {
+                temp = sourceList.get(i) * 2;
+                set.add(temp);
+            }
+        }
+        return set;
     }
 }
